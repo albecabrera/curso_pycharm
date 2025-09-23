718bc62dc66dbbef47918cf16f92736305d576de
albecabrera <albe.cabrera@yahoo.de>
2025-09-23 19:41:26 +0200

    lower und upper

--- Pythonkurs_deutsch.py
+++ Pythonkurs_deutsch.py
@@ -1,4 +1,4 @@
-# # Printbefehle
+#  Printbefehle
 # print("Hallo Welt!")
 # print()
 # print("Hallo", "Welt", "!")
@@ -11,8 +11,28 @@
 # print()
 # print("Hallo" + " " + "Welt" + "!" * 3, "Hallo" + " " + "Welt" + "!" * 3)
 # print()
+from os.path import split
 
 # Eine Variable definieren
 y = "Hallo Welt!"
-print(y)
+# Eine andere Variable definieren und die beiden Variablen zusammen ausgeben
+# Konkatenation: Verkettung von Zeichenketten
+x = "Python ist toll, oder!"
+print(y + " " + x)
+print() # Leerzeile
+# Groß- und Kleinschreibung
+print(x.upper())
+print(x.lower())
+# splitten: Aufteilen einer Zeichenkette
+print(x.split(" "))
+# an einer bestimmten Stelle einen Teil der Zeichenkette ausgeben
+print(x[0]) # Erster Buchstabe
+print(x[1]) # Zweiter Buchstabe
+print(x.split(",")) # An dem Komma wird die Zeichenkette aufgeteilt5
+print(x.split("toll")) # An dem Wort "toll" wird die Zeichenkette aufgeteilt
 print()
+
+# Ein Wort in einer Zeichenkette suchen
+a = "Hallo Alberto"
+print(a.find("Alberto"))  # Gibt die Position des Anfangs des Wortes zurück (6)
+print(a[6:13])  # Gibt den Teil der Zeichenkette ab Position 6 aus
