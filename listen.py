2e09f7c68b425596900684e01a3d62d0a31dd510
albecabrera <albe.cabrera@yahoo.de>
2025-09-27 23:29:14 +0200

    print-befehle

---
+++ listen.py
@@ -0,0 +1,18 @@
+# Listen in Python
+einfaufszettel = ['Apfel', 'Karotte', 'Banane']
+print(einfaufszettel)
+print(einfaufszettel[1]) # ein Element der Einkaufsliste ausgeben
+print(type(einfaufszettel[2])) # Datentyp festellen
+einfaufszettel.append("Nüsse") # Hinzufügen eines Elements
+print(einfaufszettel)
+einfaufszettel.insert(0, "Schokolade") # Am Anfang der Liste hinzufügen
+print(einfaufszettel)
+einfaufszettel.remove("Nüsse") # Element löschen
+print(einfaufszettel)
+einfaufszettel.pop() # Löscht das letzte Element
+print(einfaufszettel)
+del einfaufszettel[0] # Löscht das erste Element
+print(einfaufszettel)
+
+
+
